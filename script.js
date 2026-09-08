fe8b4a00e9ec7bd2378f6b4d6ac2c24958f9c6a5
Jonny Molen <jonnymolen@gmail.com>
2026-09-07 17:56:55 -0700

    Hovering Effect
    Implemented a hovering effect that leaves a blue trail for a 2 seconds.

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ const container = document.querySelector('.container');
         //adds new div element to container 
         container.append(newDiv); 
         }
+
+//hovering event listener
+const divs = document.querySelectorAll('.container div');
+
+divs.forEach((div) => {
+    //mouseover event listener to change background color of div to blue
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'blue';
+
+        //reset button after x amount of time
+        setTimeout(() => {
+            div.style.backgroundColor  = '';
+        }, 900); //resets background color after 0.9 seconds
+
+    });
+
+});
