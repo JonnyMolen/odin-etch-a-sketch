789ce99624505527da52ace69884b2fa3f445ffb
Jonny Molen <jonnymolen@gmail.com>
2026-09-08 14:18:09 -0700

    FInal Grid Implementation
    new grid allows for startup with 16 \by 16 grid and allows for user to create new grid of their choice

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const container = document.querySelector('.container');
     for (let i = 0; i < 256; i++) { 
         //creates a new div element 
         const newDiv = document.createElement("div");
+        newDiv.style.width = (6.25) + "%";
         //adds new div element to container 
         container.append(newDiv); 
         }
@@ -25,4 +26,43 @@ divs.forEach((div) => {
 
     });
 
+});
+
+
+//new grid button
+const newGridButton = document.querySelector('#new-grid');
+
+newGridButton.addEventListener('click', () => {
+    //prompt user for new grid size
+    const newSize = prompt('Enter new grid size (1-100):');
+    const size = parseInt(newSize);
+
+    //validate input
+    if (isNaN(size) || size < 1 || size > 100) {
+        alert('Invalid input. Please enter a number between 1 and 100.');
+        return;
+    }
+
+    //clear existing grid
+    container.innerHTML = '';
+
+    //create new grid based on user input
+    for (let i = 0; i < size * size; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.style.width = (100 / size) + "%";
+        container.append(newDiv);
+    }
+
+    //update divs NodeList after creating new grid
+    const divs = document.querySelectorAll('.container div');
+
+    divs.forEach((div) => {
+        div.addEventListener('mouseover', () => {  
+            div.style.backgroundColor = 'blue';
+
+            setTimeout(() => {
+                div.style.backgroundColor  = '';
+            }, 900);
+        });
+    });
 });
