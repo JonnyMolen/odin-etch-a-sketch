25ad04f393f8e6cf1432654e55b492bc9891d6da
Jonny Molen <jonnymolen@gmail.com>
2026-09-07 17:44:51 -0700

    Created a 16x16 Grid
    Utilized a for loop and flexbox for grid creation in css and js files

---
+++ script.js
@@ -0,0 +1,11 @@
+
+//initialize container
+const container = document.querySelector('.container');
+
+    //creates grid  
+    for (let i = 0; i < 256; i++) { 
+        //creates a new div element 
+        const newDiv = document.createElement("div");
+        //adds new div element to container 
+        container.append(newDiv); 
+        }
